f4ea52d4fb6b6f0004557d3843384b94b348d603
incend1ary <ayrat1989@yandex.ru>
2023-03-26 14:50:04 +0300

    homework 4

--- homeowrk4_.py
+++ homeowrk4_.py
@@ -1,41 +1,22 @@
-# Реализовать скрипт, в котором должна быть предусмотрена функция расчета заработной
-# платы сотрудника. В расчете необходимо использовать формулу: (выработка в часах*ставка в
-# час) + премия. Для выполнения расчета для конкретных значений необходимо запускать
-# скрипт с параметрами.
+from functools import reduce
+n = int(input("введите число для вычисления факториала"))
+def reduce_func(el1, el2):
+    return el1*el2
 
+new_list = [el for el in range(1,n+1)]
+ # print(reduce(reduce_func, new_list))
+gen = reduce(lambda el1, el2: el1 * el2, [el for el in range(1, n+1)])
 
-from sys import argv
-from random import randint
-def salary():
-    try:
-        time, rate, bonus = map(int, argv[1:4])
-        print(f"зарплата = {time * rate + bonus}")
-    except ValueError:
-        print("Введите 3 численных значения в скрипте")
+print(gen)
 
-
-# def bigger(list):
-#     new_list = []
-#     for i in range(1, len(list)):
-#         if list[i] > list[i-1]:
-#             new_list.append(list[i])
-#     return new_list
-
-
-def bigger(list):
-    my_list = [list[i] for i in range(1, len(list)) if list[i] > list[i-1]]
-    return my_list
-
-
-salary()
-print(bigger([1, 3, 8, 4, 9, 45, 33, 33, 313]))
-
-
-new_ = [num for num in range(20, 241) if (num % 20 == 0 or num % 21 == 0)]
-print(new_)
-
-new_list = [randint(-20, 20) for _ in range(20)]
-gen_list = [num for num in new_list if new_list.count(num) == 1]
-print(gen_list)
+# def fact_gen(n):
+#     f_num = 1
+#     if n == 0:
+#         yield f'(n)! = 1'
+#     for i in range(1, n+1):
+#         f_num *= i
+#         yield f'{i}! = {f_num}'
+# for _ in fact_gen(n):
+#     print(_)
 
 
